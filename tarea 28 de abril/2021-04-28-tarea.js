048db2560410c7a7c461dd024f23ca3e3689e2b4
Pablo Giroud <pablogiroud@gmail.com>
2021-04-28 18:54:08 -0300

    tarea 28/04/2021

---
+++ tarea 28 de abril/2021-04-28-tarea.js
@@ -0,0 +1,24 @@
+//Creamos dos variables: numero1, numero2
+let numero1 = 0;
+let numero2 = 0;
+
+//Mostramos por consola los resultados de las operaciones
+console.log('El resultado de las operaciones es:');
+
+//Sumamos dos números
+let suma = numero1 + numero2;
+
+//Restamos dos números
+let resta = numero2 - numero1;
+
+//multiplicamos dos números
+let multiplicacion = numero1 * numero2;
+
+//Dividimos dos números
+let division = numero2 / numero1;
+
+//Comparamos si el numero 1 es mayor o igual que numero 2
+console.log(numero1==numero2);
+
+//Comparamos si numero 1 es distinto de numero 2
+console.log(numero1!=numero2)
